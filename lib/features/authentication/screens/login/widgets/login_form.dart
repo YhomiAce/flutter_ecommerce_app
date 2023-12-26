8b43ba1e67823a1fc3353ea49fab915d4ee37b59
YhomiAce <kareemyomi91@gmail.com>
2023-12-26 23:47:44 +0100

    Forgot password and Reset Email

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:ecommerce_app/features/authentication/screens/signup/signup.dart';
 import 'package:ecommerce_app/utils/constants/sizes.dart';
 import 'package:ecommerce_app/utils/constants/text_strings.dart';
@@ -52,7 +53,8 @@ class LoginForm extends StatelessWidget {
 
                 // Forgot Password
                 TextButton(
-                    onPressed: () {}, child: const Text(TTexts.forgetPassword))
+                    onPressed: () => Get.to(() => const ForgotPasswordScreen()),
+                    child: const Text(TTexts.forgetPassword))
               ],
             ),
             const SizedBox(
